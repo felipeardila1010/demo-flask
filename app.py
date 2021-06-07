81a2cbae204c0da3eb73b23c85a45a2ca374dc7a
Andrew Ardila <andres@cobre.co>
2021-06-07 01:28:13 -0500

    for in template

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['TODO 1', 'TODO 2', 'TODO 3']
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -14,8 +16,12 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
 
-    return render_template('hello.html', user_ip=user_ip)
+    return render_template('hello.html', **context)
     #return 'Hello World!, tu IP es {}'.format(user_ip)
 
 if __name__ == '__main__':
