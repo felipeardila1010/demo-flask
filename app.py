0eeb9b5dd8d6a81e6d3b97fd7d6c80a972e9d16a
Andrew Ardila <andres@cobre.co>
2021-06-12 13:30:37 -0500

    Include testing and form

--- app.py
+++ app.py
@@ -1,28 +1,68 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+import unittest
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
-todos = ['TODO 1', 'TODO 2', 'TODO 3']
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
-@app.route('/hello')
+
+@app.route('/hello', methods= ['GET', 'POST'])
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
-        'todos': todos
+        'todos': todos,
+        'login_form': login_form,
+        'username': username
     }
 
-    return render_template('hello.html', **context)
-    #return 'Hello World!, tu IP es {}'.format(user_ip)
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        flash('Nombre de usuario registrado con exito')
+
+        return redirect(url_for('index'))
+
+    return render_template('hello.html', **context)
